022d72dc8aa1d48d6c6bdf209005f74a44f45793
Junior Araujo <juninho@juninho.io>
2024-05-14 19:35:38 -0300

    seed categories

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,13 +6,7 @@ async function main() {
   try {
     await database.category.createMany({
       data: [
-        { name: "Computer Science" },
-        { name: "Music" },
-        { name: "Fitness" },
-        { name: "Photography" },
-        { name: "Accounting" },
-        { name: "Engineering" },
-        { name: "Filming" },
+        { name: "Electric Guitar" },
       ]
     });
 
