a08a758cb3f30f890794dba557fdc87721140e14
Junior Araujo <juninho@juninho.io>
2024-05-14 19:25:41 -0300

    Course requirements

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/requirements-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/requirements-form.tsx
@@ -65,7 +65,7 @@ export const RequirementsForm = ({
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
-        Requirements
+        Course Requirements
         <Button onClick={toggleEdit} variant="ghost">
           {isEditing ? (
             <>Cancel</>
