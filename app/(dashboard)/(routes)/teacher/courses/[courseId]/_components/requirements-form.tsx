7c1dba98c2bd777b031a6468f964474d78c4091e
Junior Araujo <juninho@juninho.io>
2024-05-14 19:27:48 -0300

    update schema

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/requirements-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/requirements-form.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-import * as z from "zod";
-import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { Pencil } from "lucide-react";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { useRouter } from "next/navigation";
 import { Course } from "@prisma/client";
+import axios from "axios";
+import { Pencil } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import * as z from "zod";
 
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -17,9 +18,8 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
+import { cn } from "@/lib/utils";
 
 interface RequirementsFormProps {
   initialData: Course;
