3efd96022cc7c8a48ad502cd9fb0e645bf067aff
Junior Araujo <juninho@juninho.io>
2024-05-14 19:32:30 -0300

    allow all admin

--- lib/teacher.ts
+++ lib/teacher.ts
@@ -1,4 +1,10 @@
 export const isTeacher = (userId?: string | null) => {
+  // Verifica se a variável de ambiente está configurada para permitir todos como admins
+  if (process.env.NEXT_PUBLIC_ALLOW_ALL_ADMIN === 'true') {
+    return true;
+  }
+
+  // Continua com a lógica de verificação original se não estiver configurado para permitir todos
   const admins = process.env.NEXT_PUBLIC_TEACHER_IDS?.split(',');
   return admins?.includes(userId ?? '');
 }
